52cfb8c62f23910e71dbdbd1be91c48c5229f596
Brian Abelson <brianabelson@gmail.com>
2013-03-06 19:17:40 -0500

    removed .DS_Store

--- dev/example.R
+++ dev/example.R
@@ -3,12 +3,19 @@ library("devtools")
 install_github("highChaRRRts", "csvsoundsystem")
 library("highChaRRRts")
 
-d <- read.csv("/Users/brian/Desktop/wide_est.csv", as.is=FALSE)
+# generate example data...
+group <- paste0("group_", 1:11)
+var1 <- rnorm(11, mean=100, sd=30)
+var2 <- rnorm(11, mean=100, sd=30)
+var3 <- rnorm(11, mean=100, sd=30)
+var4 <- rnorm(11, mean=100, sd=30)
+var5 <- rnorm(11, mean=100, sd=30)
+df <- data.frame(group, var1, var2, var3, var4, var5)
 
-highChaRRRts(d,
-             type="bar",
-             main="I love Joanna",
+highChaRRRts(df,
+             type="column",
+             main="Title of the chart",
              xlab="X Label",
              ylab="Y Label",
-             output = "output_1",
-             pal=NULL)
+             output = "output",
+             pal="Spectral")
